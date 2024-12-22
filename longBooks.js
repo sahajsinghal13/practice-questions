9c9101840f377dd76e696a3997bccbe698bf2ee5
Sahaj <183721605+sahajsinghal13@users.noreply.github.com>
2024-12-22 14:12:53 +0530

    Added filter books longer than 200 pages

---
+++ longBooks.js
@@ -0,0 +1,11 @@
+// books with more than 200 pages [{title: "Book 1", pages: 150}, {title: "Book 2", pages: 250}] => [{title: "Book 2", pages: 250}]
+const isGreater = function (bookSpecs) {
+  return bookSpecs.pages > 200;
+};
+
+const filterLongBooks = function (books) { 
+  return books.filter(isGreater);
+};
+
+console.log(filterLongBooks([{title: "Book 1", pages: 150}, 
+  {title: "Book 2", pages: 250}, {title: "Book 3", pages: 200}]));
